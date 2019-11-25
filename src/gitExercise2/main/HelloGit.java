403b533c77e8a91fcbd548296a3bb5e0d2185919
steber <sbertolotti@myco.it>
2019-11-25 18:12:40 +0100

    Nuova stampa

--- src/gitExercise2/main/HelloGit.java
+++ src/gitExercise2/main/HelloGit.java
@@ -3,5 +3,6 @@ package gitExercise2.main;
 public class HelloGit {
 	public static void main (String [] args) {
 		System.out.println("Hello git!!!");
+		System.out.println("Questa è una stampa!")
 	}
 }
